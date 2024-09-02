41eb82a75a0e83b24f0336f3508c170ef435421f
Raryel C. Souza <raryel.costa@gmail.com>
2024-09-02 17:06:35 -0300

    fix paths and remove additional parameters

--- wmj/cron_prod_wmj.sh
+++ wmj/cron_prod_wmj.sh
@@ -8,7 +8,7 @@ $script_dir/../lib/update_cron.sh "0 2 * * * "$script_dir/"backup_etc_dir.sh"
 $script_dir/../lib/update_cron.sh "0 2 * * * "$script_dir/"backup-wp_files_and_DB.sh"
 $script_dir/../lib/update_cron.sh "0 0 * * * "$script_dir/"monitor_disk_usage_wmj.sh"
 $script_dir/../lib/update_cron.sh "0 0 * * * "$script_dir/../lib/"monitor_logs_unattended_upgrades.sh"
-$script_dir/../lib/update_cron.sh "30 3 1 * * "$script_dir/../lib/"clean_script_logs_older_n_days.sh" 90
+$script_dir/../lib/update_cron.sh "30 3 1 * * "$script_dir/../lib/"clean_script_logs_older_n_days.sh"
 
 
 $script_dir/../lib/check_if_error_log_not_empty.sh $ERROR_LOG_FILE
--- wmj/cron_qa_wmj.sh
+++ wmj/cron_qa_wmj.sh
@@ -6,10 +6,10 @@ source "$script_dir/../creds/duplicity.sh"
 
 
 $script_dir/../lib/cron_qa_schedule.sh $script_dir/"backup_etc_dir.sh"
-$script_dir/../lib/cron_qa_schedule.sh $script_dir/"backup-wp_files_and_DB.sh"
+$script_dir/../lib/cron_qa_schedule.sh $script_dir/"backup_wp_files_and_DB.sh"
 $script_dir/../lib/cron_qa_schedule.sh $script_dir/"monitor_disk_usage_wmj.sh"
 $script_dir/../lib/cron_qa_schedule.sh $script_dir/../lib/"monitor_logs_unattended_upgrades.sh"
-$script_dir/../lib/cron_qa_schedule.sh $script_dir/../lib/"clean_script_logs_older_n_days.sh" 90
+$script_dir/../lib/cron_qa_schedule.sh $script_dir/../lib/"clean_script_logs_older_n_days.sh"
 
 
 $script_dir/../lib/check_if_error_log_not_empty.sh $ERROR_LOG_FILE
