16e4793ab1d9a4efd30988424c1f03268186f840
Raryel C. Souza <raryel.costa@gmail.com>
2024-09-02 17:05:06 -0300

    hardcode 90 days as log rotation period to simplify on cron scripts

--- lib/clean_script_logs_older_n_days.sh
+++ lib/clean_script_logs_older_n_days.sh
@@ -3,20 +3,8 @@
 script_dir="$(dirname "$(readlink -f "$0")")"
 source "$script_dir/../lib/vars_logging.sh"
 
-# Check if the number of days argument is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <number_of_days>"
-    exit 1
-fi
-
 # Retrieve the number of days from the argument
-days="$1"
-
-# Validate if the argument is a positive integer
-if ! [[ "$days" =~ ^[0-9]+$ ]] || [ "$days" -le 0 ]; then
-    echo "Error: <number_of_days> must be a positive integer."
-    exit 1
-fi
+days=90
 
 # Determine the directory of the current script
 script_dir="$(dirname "$(readlink -f "$0")")"
