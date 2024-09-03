8fc6f123bbdf16d8ee05874809305e51efa797a7
Raryel C. Souza <raryel.costa@gmail.com>
2024-09-03 01:00:45 -0300

    separate cron for wmj prod in normal user and root

--- wmj/cron_prod_root.sh
+++ wmj/cron_prod_root.sh
@@ -4,8 +4,8 @@ script_dir="$(dirname "$(readlink -f "$0")")"
 source "$script_dir/../lib/vars_logging.sh"
 source "$script_dir/../creds/duplicity.sh"
 
-$script_dir/../lib/update_cron.sh "0 2 * * * /bin/bash -c "$script_dir/"backup-wp_files_and_DB.sh"
-$script_dir/../lib/update_cron.sh "0 0 * * * /bin/bash -c "$script_dir/"monitor_disk_usage_wmj.sh"
+$script_dir/../lib/update_cron.sh "0 1 * * * /bin/bash -c "$script_dir/"backup_etc_dir.sh"
+$script_dir/../lib/update_cron.sh "0 0 * * * /bin/bash -c "$script_dir/../lib/"monitor_logs_unattended_upgrades.sh"
 $script_dir/../lib/update_cron.sh "30 3 1 * * /bin/bash -c "$script_dir/../lib/"clean_script_logs_older_n_days.sh"
 
 
