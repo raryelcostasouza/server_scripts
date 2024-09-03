b97e6c62b20d04dce7e2158b235effbffba2def2
Raryel C. Souza <raryel.costa@gmail.com>
2024-09-03 01:11:07 -0300

    replace / with /dev/sda1 to be specific

--- flux/monitor_disk_usage_flux.sh
+++ flux/monitor_disk_usage_flux.sh
@@ -3,5 +3,5 @@
 script_dir="$(dirname "$(readlink -f "$0")")"
 source $script_dir/../lib/vars_logging.sh
 
-$script_dir/../lib/monitor_disk_usage.sh 80 /
+$script_dir/../lib/monitor_disk_usage.sh 80 /dev/sda1
 
